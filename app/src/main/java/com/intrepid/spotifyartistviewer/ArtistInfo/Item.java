67e55280bb5066c5a2fa2cc5b23574a47bcaf46c
Memo <memobetl@gmail.com>
2016-06-06 19:23:55 -0400

    First commit

---
+++ app/src/main/java/com/intrepid/spotifyartistviewer/ArtistInfo/Item.java
@@ -0,0 +1,265 @@
+
+package com.intrepid.spotifyartistviewer.ArtistInfo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Generated("org.jsonschema2pojo")
+@JsonPropertyOrder({
+    "external_urls",
+    "followers",
+    "genres",
+    "href",
+    "id",
+    "images",
+    "name",
+    "popularity",
+    "type",
+    "uri"
+})
+public class Item {
+
+    @JsonProperty("external_urls")
+    private ExternalUrls externalUrls;
+    @JsonProperty("followers")
+    private Followers followers;
+    @JsonProperty("genres")
+    private List<Object> genres = new ArrayList<Object>();
+    @JsonProperty("href")
+    private String href;
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("images")
+    private List<Image> images = new ArrayList<Image>();
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("popularity")
+    private Integer popularity;
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("uri")
+    private String uri;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * 
+     * @return
+     *     The externalUrls
+     */
+    @JsonProperty("external_urls")
+    public ExternalUrls getExternalUrls() {
+        return externalUrls;
+    }
+
+    /**
+     * 
+     * @param externalUrls
+     *     The external_urls
+     */
+    @JsonProperty("external_urls")
+    public void setExternalUrls(ExternalUrls externalUrls) {
+        this.externalUrls = externalUrls;
+    }
+
+    /**
+     * 
+     * @return
+     *     The followers
+     */
+    @JsonProperty("followers")
+    public Followers getFollowers() {
+        return followers;
+    }
+
+    /**
+     * 
+     * @param followers
+     *     The followers
+     */
+    @JsonProperty("followers")
+    public void setFollowers(Followers followers) {
+        this.followers = followers;
+    }
+
+    /**
+     * 
+     * @return
+     *     The genres
+     */
+    @JsonProperty("genres")
+    public List<Object> getGenres() {
+        return genres;
+    }
+
+    /**
+     * 
+     * @param genres
+     *     The genres
+     */
+    @JsonProperty("genres")
+    public void setGenres(List<Object> genres) {
+        this.genres = genres;
+    }
+
+    /**
+     * 
+     * @return
+     *     The href
+     */
+    @JsonProperty("href")
+    public String getHref() {
+        return href;
+    }
+
+    /**
+     * 
+     * @param href
+     *     The href
+     */
+    @JsonProperty("href")
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    /**
+     * 
+     * @return
+     *     The id
+     */
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * 
+     * @param id
+     *     The id
+     */
+    @JsonProperty("id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 
+     * @return
+     *     The images
+     */
+    @JsonProperty("images")
+    public List<Image> getImages() {
+        return images;
+    }
+
+    /**
+     * 
+     * @param images
+     *     The images
+     */
+    @JsonProperty("images")
+    public void setImages(List<Image> images) {
+        this.images = images;
+    }
+
+    /**
+     * 
+     * @return
+     *     The name
+     */
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * @param name
+     *     The name
+     */
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 
+     * @return
+     *     The popularity
+     */
+    @JsonProperty("popularity")
+    public Integer getPopularity() {
+        return popularity;
+    }
+
+    /**
+     * 
+     * @param popularity
+     *     The popularity
+     */
+    @JsonProperty("popularity")
+    public void setPopularity(Integer popularity) {
+        this.popularity = popularity;
+    }
+
+    /**
+     * 
+     * @return
+     *     The type
+     */
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * 
+     * @param type
+     *     The type
+     */
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * 
+     * @return
+     *     The uri
+     */
+    @JsonProperty("uri")
+    public String getUri() {
+        return uri;
+    }
+
+    /**
+     * 
+     * @param uri
+     *     The uri
+     */
+    @JsonProperty("uri")
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
